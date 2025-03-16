4d0e46f541926456bdeeabaa5e84c867319d89b0
sungchanyoung <112501530+sungchanyoung@users.noreply.github.com>
2025-03-17 01:44:31 +0900

    Update App.java

--- untitled/src/main/java/com/korit/strategyPattern/App.java
+++ untitled/src/main/java/com/korit/strategyPattern/App.java
@@ -1,4 +1,12 @@
 package com.korit.strategyPattern;
 
 public class App {
+  //dip도 들어가 있음 - 추상화 구제적인것들응 추상화로 바라본다.
+    public static void main(String[] args) {
+        Mouse m = new Mouse();
+        Cat c = new Cat();
+        Doorman dm = new Doorman();
+        dm.getout(m);
+        dm.getout(c);
+    }
 }
